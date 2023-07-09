fb2ab0279e5736fadc05afed12eee161b398a27c
Arthur Belini Pini <ra118999@uem.br>
2023-07-09 15:30:59 -0300

    Código funcionando

--- mm_p.c
+++ mm_p.c
@@ -1,12 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
-void *f(void *t_id) {
+#include "mm_aux.h"
 
-}
+// m3 = m1*m2
+float **m1;
+float **m2;
+float **m3;
+int tam_m;
+int qtd_t;
+
+void *mm(void *t_id);
+void start_threads();
    
-int main() {
-    
+int main(int argc, char *argv[]) {
+    pthread_t *ts;
+    int *ts_ids;
+
+    if(argc != 3) {  // tam matriz - qtd threads
+        printf("Quantidade de argumentos em linha de comando inválida!\n");
+        exit(1);
+    }
+
+    tam_m = strtol(argv[1], NULL, 10);
+    qtd_t = strtol(argv[2], NULL, 10);
+
+    // printf("%d", tam_m);
+    if(tam_m <= 0 || qtd_t <= 0) {
+        printf("Valores de argumentos em linha de comando inválidos!\n");
+        exit(1);
+    }
+
+    alocate_matrix(&m3, tam_m);
+    fill_matrix(&m1, tam_m);
+    fill_matrix(&m2, tam_m);
+
+    // printf("\n");
+
+    ts = (pthread_t*) calloc(qtd_t, sizeof(pthread_t));
+    ts_ids = (int*) calloc(qtd_t, sizeof(int));
+    for(int i = 0; i < qtd_t; i++) {
+        ts_ids[i] = i;
+
+        pthread_create(&(ts[i]), NULL, &mm, &ts_ids[i]);
+    }
+    for(int i = 0; i < qtd_t; i++) {
+        pthread_join(ts[i], NULL);
+    }
 
     exit(0);
+}
+
+void *mm(void *t_id) {
+    for(int i = *(int*)t_id; i < tam_m; i+=qtd_t) {  // Linha
+        for(int j = 0; j < tam_m; j++) {  // Coluna
+            for(int k = 0; k < tam_m; k++) {  // Elementos
+                m3[i][j] += m1[i][k] * m2[k][j];
+            }
+            // printf("%f ", m3[i][j]);
+        }
+        // printf("\n");
+    }
 }
--- mm_s.c
+++ mm_s.c
@@ -1,32 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 #include "mm_aux.h"
 
+// m3 = m1*m2
 float **m1;
 float **m2;
 float **m3;
 int tam_m;
-int qtd_t;
+
+void mm();
    
 int main(int argc, char *argv[]) {
-    if(argc != 3) {  // tam matriz - qtd threads
+    if(argc != 2) {  // tam matriz - qtd threads
+        printf("Quantidade de argumentos em linha de comando inválida!\n");
         exit(1);
     }
 
     tam_m = strtol(argv[1], NULL, 10);
-    qtd_t = strtol(argv[2], NULL, 10);
 
-    if(tam_m <= 0 || qtd_t <= 0) {
+    // printf("%d", tam_m);
+    if(tam_m <= 0) {
+        printf("Valor de argumento em linha de comando inválido!\n");
         exit(1);
     }
 
-    fill_matrix(m1, tam_m);
-    // fill_matrix(m2, tam_m);
+    alocate_matrix(&m3, tam_m);
+    fill_matrix(&m1, tam_m);
+    fill_matrix(&m2, tam_m);
 
-    // printf("%d\n", m1[0][0]);
-    // printf("%d %d\n", tam_m, qtd_t);
+    mm();
 
     exit(0);
+}
+
+void mm() {
+    float x;
+
+    for(int i = 0; i < tam_m; i++) {  // Linha
+        printf("\n");
+        for(int j = 0; j < tam_m; j++) {  // Coluna
+            for(int k = 0; k < tam_m; k++) {  // Elementos
+                m3[i][j] += m1[i][k] * m2[k][j];
+            }
+            printf("%f ", m3[i][j]);
+        }
+    }
+    printf("\n");
 }
